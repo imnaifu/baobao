ae7d82039a199323e57aa57d253065648a119c0c
imnaifu <naifu_xu@outlook.com>
2017-07-08 15:25:35 +0800

    et

--- core/baobao.php
+++ core/baobao.php
@@ -18,10 +18,10 @@ class baobao
 		self::remove_magicquotes();
 		self::unregister_globals();
 
-		// $log = new \core\library\baobao_log();
-		// $log->start_daily_log();
+		$log = new \core\library\baobao_log;
+		$log->start_daily_log();
 
-		$route = new \core\library\route();
+		$route = new \core\library\route;
 		$controller = $route->controller;
 		$action = $route->action;
 
